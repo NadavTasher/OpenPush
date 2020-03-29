ec823542987e31ed34922c971892855369f7ad41
nadav <tashernadav@gmail.com>
2020-03-29 23:56:52 +0300

    Updates

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,33 @@
-function generate(service = "OpenPush GUI") {
+function generate() {
+    token((token) => {
+        let URL = window.location.origin + "/apis/notify/?api=" + (JSON.stringify({
+            notify: {
+                action: "notify",
+                parameters: {
+                    title: "Your title",
+                    message: "Your message",
+                    token: token
+                }
+            }
+        }));
+        // Show the URL to the user
+        window.open(URL, "_blank");
+    });
+}
+
+function issue() {
+    token((token) => {
+        prompt("Copy your token:", token);
+    });
+}
+
+function token(callback = null) {
     API.send("notify", "issue", {
-        application: service
+        application: prompt("Enter your service name:", "OpenPush GUI")
     }, (success, token) => {
         if (success) {
-            let URL = window.location.origin + "/apis/notify/?api=" + (JSON.stringify({
-                notify: {
-                    action: "notify",
-                    parameters: {
-                        title: "Your title",
-                        message: "Your message",
-                        token: token
-                    }
-                }
-            }));
-            // Show the URL to the user
-            window.open(URL, "_blank");
+            if (callback !== null)
+                callback(token);
         }
     }, Authenticate.authenticate());
 }
