862b925c7bcb31731596747eccc6796f0e92850e
nadav <tashernadav@gmail.com>
2020-04-01 15:15:53 +0300

    Platform update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,36 +1,45 @@
 function load() {
+    UI.view("home");
 }
 
 function generate() {
-    token((token) => {
-        let URL = window.location.origin + "/apis/push/?api=" + (JSON.stringify({
-            push: {
-                action: "push",
-                parameters: {
-                    title: "Your title",
-                    message: "Your message",
-                    token: token
-                }
+    let application = prompt("Enter your service name:", "OpenPush GUI");
+    if (application !== null && application.length > 0) {
+        API.send("push", "issue", {
+            application: application
+        }, (success, token) => {
+            if (success) {
+                // Create URL
+                let url = window.location.origin + "/apis/push/?api=" + (JSON.stringify({
+                    push: {
+                        action: "push",
+                        parameters: {
+                            title: "Your title",
+                            message: "Your message",
+                            token: token
+                        }
+                    }
+                }));
+                // Set values and onclicks
+                let tokenHolder = UI.find("token");
+                let urlHolder = UI.find("url");
+                // Set value
+                tokenHolder.value = token;
+                urlHolder.value = url;
+                // Set onclick
+                tokenHolder.addEventListener("click", function () {
+                    tokenHolder.select();
+                    tokenHolder.setSelectionRange(0, tokenHolder.value.length);
+                    document.execCommand("copy");
+                });
+                urlHolder.addEventListener("click", function () {
+                    urlHolder.select();
+                    urlHolder.setSelectionRange(0, urlHolder.value.length);
+                    document.execCommand("copy");
+                });
+                // Change view
+                UI.view("result");
             }
-        }));
-        // Show the URL to the user
-        window.open(URL, "_blank");
-    });
-}
-
-function issue() {
-    token((token) => {
-        prompt("Copy your token:", token);
-    });
-}
-
-function token(callback = null) {
-    API.send("push", "issue", {
-        application: prompt("Enter your service name:", "OpenPush GUI")
-    }, (success, token) => {
-        if (success) {
-            if (callback !== null)
-                callback(token);
-        }
-    }, Authenticate.authenticate());
+        }, Authenticate.authenticate());
+    }
 }
